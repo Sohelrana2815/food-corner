d1854f57e4325e1aacf48a781e46c94856d82db7
Sohelrana2815 <sohel152302@gmail.com>
2024-11-23 19:56:13 +0600

    show some data

--- src/Pages/Home/Recipes/RecipeCard.jsx
+++ src/Pages/Home/Recipes/RecipeCard.jsx
@@ -1,14 +1,20 @@
 const RecipeCard = ({ recipe }) => {
-  const { recipe_image, recipe_name, description } = recipe;
+  const { recipe_image, recipe_name, description, ingredients } = recipe;
   return (
     <>
       <div className="card card-compact  lg:w-50 bg-base-100 shadow-xl xl:w-72">
-        <figure>
-          <img src={recipe_image} alt="Shoes" />
+        <figure className="h-56">
+          <img className="rounded-lg" src={recipe_image} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="md:text-lg font-semibold">{recipe_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>{description}</p>
+          <h4>Ingredients: {ingredients.length}</h4>
+          <ul>
+            {ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
